3fdb1975c0d61f0da3e5f0b77d2e3e669f831bf4
Maharab Kibria <junakkibria56@gmail.com>
2023-06-12 12:08:05 +0600

    leetcode submitted for goodPairs beating 100%

--- src/designgurus/GoodPairs.java
+++ src/designgurus/GoodPairs.java
@@ -1,12 +1,17 @@
 package designgurus;
+import java.util.Map;
+import java.util.HashMap;
 
 public class GoodPairs {
     public  static void main(String[]args){
         GoodPairs gp = new GoodPairs();
-//        int [] nums = {1,2,3,1,1,3};
-        int [] nums = {1,2,3};
+        int [] nums = {1,2,3,1,1,3};
+//        int [] nums = {1,2,3};
         System.out.println(gp.getGoodPairs(nums));
+        System.out.println(gp.getGoodPairsFreqApproach(nums));
     }
+
+    //naive approach at first thought. worst complexity.
     public int getGoodPairs(int [] nums){
     int output = 0;
     for (int i = 0; i < nums.length-1; i++){
@@ -17,5 +22,17 @@ public class GoodPairs {
         }
     }
     return output;
+    }
+
+    //better approach.
+    public int getGoodPairsFreqApproach(int [] nums){
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        int pairCount = 0;
+        //populate frequency map
+        for (int i : nums) {
+            freqMap.put(i, freqMap.getOrDefault(i, 0) + 1);
+            pairCount+=freqMap.get(i)-1;
+        }
+    return pairCount;
     }
 }
